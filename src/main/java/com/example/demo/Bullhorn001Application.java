d780b82d91495d76bb4c557e060905719160dec9
GBTC405004UR <GBTC405004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-22 16:53:42 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Bullhorn001Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bullhorn001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bullhorn001Application.class, args);
+    }
+
+}
